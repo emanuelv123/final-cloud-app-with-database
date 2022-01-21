6ade54d6400aaa2f7f430c40799326ab7e864d0a
Emanuelv248 <verteikoemanuel@gmail.com>
2022-01-21 20:01:39 +0000

    changes made from theia lab environment

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -16,8 +16,11 @@ urlpatterns = [
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
     # ex: /enroll/5/
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
+    path('<int:course_id>/submit/', views.submit_view, name='submit_view'), # <HINT> Create a route for submit view
+    # path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
 
-    # <HINT> Create a route for submit view
+
 
     # <HINT> Create a route for show_exam_result view
 
